05daced8ef119882e8331b803ade456019aebdb3
Erminio Mendes <55849740+erminiomendes@users.noreply.github.com>
2021-07-27 14:18:01 -0700

    eddited

--- typewriter.js
+++ typewriter.js
@@ -7,4 +7,4 @@ for (const char of sentence) {
   process.stdout.write(char);// print the char here
   }, timer += 50) // <= 1s delay to make it noticeable. Can dial it down later.
 }
-setTimeout(()=> {process.stdout.write("\n");}, t);
+setTimeout(()=> {process.stdout.write("\n");}, timer);
